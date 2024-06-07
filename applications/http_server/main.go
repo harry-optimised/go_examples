3f78996f5bcaa97a4f18d41fe3457313a809528c
Harry <henry.j.turner@gmail.com>
2024-06-07 07:06:40 +0100

    Completed up to part way through JSON, routing and embedding.

--- applications/http_server/main.go
+++ applications/http_server/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"http_server/server"
 	"log"
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
-	server := &server.PlayerServer{&InMemoryPlayerStore{}}
+	server := NewPlayerServer(NewInMemoryPlayerStore())
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
